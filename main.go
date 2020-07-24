3722e416e5a5dc618f4323d15b6b88fce1a06c65
Sumedh Shakya <sumedhshakya11@gmail.com>
2020-07-24 12:15:10 +0545

    master: versioning migration file with unix time stamp

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/gin-gonic/gin"
 
@@ -13,17 +14,67 @@ import (
 	"github.com/SakyaSumedh/go_migration_rnd/models"
 )
 
+type DBConfig struct {
+	Host     string
+	Port     string
+	User     string
+	DBName   string
+	Password string
+	DBType   string
+}
+
+func buildDBConfig(host, port, user, name, password string, dbType string) *DBConfig {
+	dbConfig := DBConfig{
+		Host:     host,
+		Port:     port,
+		User:     user,
+		DBName:   name,
+		Password: password,
+		DBType:   dbType,
+	}
+	return &dbConfig
+}
+
+func dbURL(dbConfig *DBConfig) string {
+	if dbConfig.DBType == "cloudsql" {
+		return fmt.Sprintf(
+			"%s:%s@unix(/cloudsql/%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+			dbConfig.User,
+			dbConfig.Password,
+			dbConfig.Host,
+			dbConfig.DBName,
+		)
+	}
+
+	return fmt.Sprintf(
+		"%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		dbConfig.User,
+		dbConfig.Password,
+		dbConfig.Host,
+		dbConfig.Port,
+		dbConfig.DBName,
+	)
+}
+
 func main() {
 	r := gin.Default()
 
-	m, err := migrate.New(
-		"file://migrations/",
-		"mysql://root:p@ssw0rd@tcp(localhost:3306)/test-gorm")
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbPort := os.Getenv("DB_PORT")
+	dbHost := os.Getenv("DB_HOST")
+	dbName := os.Getenv("DB_NAME")
+	dbType := os.Getenv("DB_TYPE")
+
+	fmt.Println("--- Connecting Migrations ---")
+	dbConfig := buildDBConfig(dbHost, dbPort, dbUser, dbName, dbPassword, dbType)
+	dbURL := dbURL(dbConfig)
+
+	m, err := migrate.New("file://api/migrations/", dbType+"://"+dbURL)
 	if err != nil {
-		fmt.Println(err.Error())
-		return
+		panic(err)
 	}
-	fmt.Println("Migration up")
+	fmt.Println("--- Running Migration ---")
 	m.Steps(1000)
 
 	models.ConnectDatabase()
